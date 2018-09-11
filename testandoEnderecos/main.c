51446c4393229cb9c70effbbe82bf5b2ad8c12e3
Ariel Alsina <ariel.alsina2007@gmail.com>
2018-09-11 19:04:23 -0300

    Novas Alterações

--- testandoEnderecos/main.c
+++ testandoEnderecos/main.c
@@ -1,20 +1,51 @@
 #include <stdio.h>
 
 int main (void) {
-    double vetor[10];
+
+    char vetor1[10];
+    int vetor2[10];
+    float vetor3[10];
+    double vetor4[10];
+
     int i;
 
     for (i=0; i<10; i++) {
-        vetor[i] = i;
+        vetor1[i] = i;
+        vetor2[i] = i;
+        vetor3[i] = i;
+        vetor4[i] = i;
     }
 
     double* p;
 
-    p = &vetor[0]; //&vetor também daria certo!
+    char* p1 = &vetor1[0]; //&vetor também daria certo!
+    int* p2 = &vetor2[0];
+    float* p3 = &vetor3[0];
+    double* p4 = &vetor4[0];
 
+    printf("Para um vetor de char: \n");
     for(i=0; i<10; i++) {
-        printf("%d\n", p+i);
+        printf("%d\n", p1+i);
     }
+    printf("\n\n");
+
+    printf("Para um vetor de int: \n");
+    for(i=0; i<10; i++) {
+        printf("%d\n", p2+i);
+    }
+    printf("\n\n");
+
+    printf("Para um vetor de float: \n");
+    for(i=0; i<10; i++) {
+        printf("%d\n", p3+i);
+    }
+    printf("\n\n");
+
+    printf("Para um vetor de double: \n");
+    for(i=0; i<10; i++) {
+        printf("%d\n", p4+i);
+    }
+    printf("\n\n");
 
     //o endereço do ponteiro, numericamente, incrementa de acordo com
     //o tamanho de minha variável em bytes, i.e, se &vetor[0] = 1000 e
